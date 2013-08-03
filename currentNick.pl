d531583dd75e89624e8a71d9c6401672b282817c
Jeff Haak <zemon1@csh.rit.edu>
2013-08-02 22:38:19 -0400

    hopefully this works?

--- currentNick.pl
+++ currentNick.pl
@@ -11,7 +11,7 @@
 # REQUIREMENTS:  ---
 #         BUGS:  ---
 #        NOTES:  ---
-#       AUTHOR:  YOUR NAME (), 
+#       AUTHOR:  Jeff Haak
 #      COMPANY:  
 #      VERSION:  1.0
 #      CREATED:  08/02/2013 02:57:46 PM
@@ -25,19 +25,25 @@ use File::ChangeNotify;
 
 
 my $username = $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
-my $filename = $username . 'conf'
+my $filename = $username . 'conf';
 
 print $username;
 
 
 my $watcher = File::ChangeNotify->instantiate_watcher(
-    directories => ['/tmp/grooveirssi'],
-    filter      => qr/m\/$filename\//,
+    directories    => ['/tmp/grooveirssi'],
+    filter         => qr/m\/$filename\//,
+    sleep_interval => 5,
 );
 
+if (my @events = $watcher->new_events()) {  
+    print $event->path(), ' - ', $event->type(), "\n";   
+}
 
 
-
+while (my @events = $watcher->wait_for_events){ 
+    print $event->path(), ' - ', $event->type(), "\n";   
+}
 
 
 
